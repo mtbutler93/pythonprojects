30047aef0011db827c2a8d9992321b8ef0d4a66b
mtbutler93 <33134486+mtbutler93@users.noreply.github.com>
2018-01-30 14:09:40 -0800

    Add files via upload

---
+++ stripspaces.py
@@ -0,0 +1,11 @@
+#program strips whitespace from a stored name
+#mtb 01.30.2018
+name = "  Michael Taylor Butler         "
+print(name.rstrip())
+
+name = "  Michael Taylor Butler         "
+print(name.lstrip())
+
+name = "         Michael Taylor Butler         "
+print(name.strip())
+
