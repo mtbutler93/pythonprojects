6230f072c5f8d33a62941307f2eb12871070a29f
mtbutler93 <33134486+mtbutler93@users.noreply.github.com>
2018-01-30 14:10:19 -0800

    Add files via upload

---
+++ happy_birthday.py
@@ -0,0 +1,6 @@
+#program stores a name, age and prints a happy birthday message 
+#mtb 01.30.2018
+age = 23
+name = "Porkchop"
+message="Happy " + str(age) +"rd Birthday " + name +"!"  
+print (message) 
