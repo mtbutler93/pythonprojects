a9c3476020bbd1c0ef36fb3ef550aa83b0d9b65b
mtbutler93 <33134486+mtbutler93@users.noreply.github.com>
2018-01-30 14:40:27 -0800

    Add files via upload
    simple list of names with a custom message

---
+++ names.py
@@ -0,0 +1,5 @@
+#list of names on 2018 japan ski trip
+#mtb 1.30.2018 
+names =['eric','gary','paul','chris','harald']
+message = names[-1].title() + " excited about our hokiaddo ski trip. Is " + names[0].title() + " coming?"
+print(message) 
