f2a445c310965d5e753a6f9339f5d09bb6586326
mtbutler93 <33134486+mtbutler93@users.noreply.github.com>
2018-02-27 15:43:04 -0800

    Add files via upload

---
+++ cars_sort.py
@@ -0,0 +1,21 @@
+# playing with lists, sort list
+# mtb 02.27.2018
+cars =['bmw','audi','porsche','vw','honda']
+print (cars)
+cars.sort()
+print (cars)
+
+cars.sort(reverse=True)
+print (cars) 
+
+cars2 =['bmw','audi','toyota','subaru']
+print (cars2)
+
+print('Here is the original list:')
+print(cars2)
+
+print('\nHere is the sorted list:')
+print(sorted(cars2))
+
+print('\nHere is the original list again:')
+print(cars2)
