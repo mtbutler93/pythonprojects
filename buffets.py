98475539bfce13efc6846c0cb9c025667e1d7d25
mtbutler93 <33134486+mtbutler93@users.noreply.github.com>
2018-03-20 13:14:11 -0700

    Add files via upload

---
+++ buffets.py
@@ -0,0 +1,11 @@
+# playing with lists, creating tuples
+# mtb 03.20.2018
+buffets = ('eegs','bacon','waffles','omlette','sausage')
+print ('Original buffet items:') 
+for buffet in buffets:
+	print(buffet)
+
+buffets = ('pizza','toast','waffles','omlette','sausage')
+print ('\nUpdated buffet items:')	
+for buffet in buffets:
+	print(buffet)
---
+++ dimensions.py
@@ -0,0 +1,8 @@
+# playing with lists, creating tuples
+# mtb 03.12.2018
+dimensions = (200,50,75)
+print(dimensions[0])
+print(dimensions[1])
+
+for dimension in dimensions:
+	print(dimension)
