36220315c5e71785d36e0789a4d38f848ba819b6
mtbutler93 <33134486+mtbutler93@users.noreply.github.com>
2018-12-06 09:56:04 -0800

    alien invasion game
    building a space invaders game using pygame and python

---
+++ alien_invasion.py
@@ -0,0 +1,42 @@
+# Alien Invasion Game Chapter 12
+# 11.2.2018
+
+import sys
+
+import pygame
+from pygame.sprite import Group
+
+from settings import Settings
+from ship import Ship
+import game_functions as gf
+
+def run_game():
+    # Initialize pygame, settings and screen object.
+    pygame.init()
+    ai_settings = Settings()
+    screen = pygame.display.set_mode(
+    (ai_settings.screen_width, ai_settings.screen_height))
+    pygame.display.set_caption("Alien Invasion")
+    
+    # Make a ship, a group of bullets, and a group of aliens
+    ship = Ship(ai_settings, screen)
+    bullets = Group()
+    aliens = Group()
+    
+    # Create the fleet of aliens
+    gf.create_fleet(ai_settings, screen, aliens)
+        
+    # Set the background color 
+    bg_color = (230, 230,230)
+    
+    # Start the main loop for the game.
+    while True:
+        gf.check_events(ai_settings, screen, ship, bullets)
+        ship.update()
+        gf.update_bullets(bullets)
+        gf.update_screen(ai_settings, screen, ship, aliens, bullets)
+        
+run_game()
+
+    
+    
