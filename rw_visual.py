76cf0e156f5fd1d71dae901c2ae8c0b0bf6c0678
Michael Butler <33134486+mtbutler93@users.noreply.github.com>
2019-09-11 16:22:12 -0700

    Random walk generator

---
+++ rw_visual.py
@@ -0,0 +1,20 @@
+import matplotlib.pyplot as plt
+
+from random_walk import RandomWalk
+
+#Keep making new walks, as long as the program is active
+while True:
+	#Make random walk and plot the points
+	rw = RandomWalk()
+	rw.fill_walk()
+	
+	point_numbers = list(range(rw.num_points))
+	plt.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues, edgecolor='none', s=15)
+	plt.show()
+
+	keep_running = input("Make another walk? (y/n): ")
+	if keep_running == 'n':
+		break
+	
+
+
